ff21cb23a25c9bdcfbc62737e78eb2e45153c3d6
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-06-16 01:09:42 +0300

    Remove outdated image from ETC_2024_IMAGES array and clean up the list for better accuracy.

--- src/lib/etc-2024-images.ts
+++ src/lib/etc-2024-images.ts
@@ -1,7 +1,6 @@
 const BASE = "https://files.ibtikar.tr/events/2304/etc-2024"
 
 export const ETC_2024_IMAGES = [
-  "20240217_152156.jpg",
   "20240217_183639.jpg",
   "photo_2024-02-26_22-31-32.jpg",
   "photo_2024-02-26_22-35-12.jpg",
@@ -10,8 +9,7 @@ export const ETC_2024_IMAGES = [
   "photo_2026-06-16_00-36-47.jpg",
   "photo_2026-06-16_00-36-51.jpg",
   "photo_2026-06-16_00-38-22.jpg",
-  "photo_2026-06-16_00-38-32.jpg",
-  "photo_2026-06-16_00-43-43.jpg",
+  "photo_2026-06-16_00-38-32.jpg"
 ] as const
 
 export function etc2024ImageUrl(filename: string) {
